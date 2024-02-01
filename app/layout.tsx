bf47364cd6fc4b6e7834980ed4e03f5aea6bb968
Mathias Ebner <fhs47758@fh-salzburg.ac.at>
2024-02-01 11:22:18 +0100

    add basic authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,12 @@ import { GeistSans } from "geist/font/sans";
 import "./globals.css";
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
 
+import { getServerSession } from "next-auth";
+import SessionProvider from "@/components/SessionProvider";
+import NavMenu from "@/components/NavMenu";
+import { options } from "./api/auth/[...nextauth]/options";
+
+
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
   : "http://localhost:3000";
@@ -12,18 +18,23 @@ export const metadata = {
   description: "The fastest way to build apps with Next.js and Supabase",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerSession(options);
+  console.log("My session: ", session)
+
   return (
     <html lang="en" className={GeistSans.className}>
       <body className="bg-background text-foreground">
-        <main className="min-h-screen flex flex-col items-center">
-          {/* {children} */}
-          <AppRouterCacheProvider options={{ enableCssLayer: true }}>{children}</AppRouterCacheProvider>
-        </main>
+        <SessionProvider session={session}>
+          <main className="min-h-screen flex flex-col items-center">
+            <NavMenu />
+            <AppRouterCacheProvider options={{ enableCssLayer: true }}>{children}</AppRouterCacheProvider>
+          </main>
+        </SessionProvider>
       </body>
     </html>
   );
---
+++ prisma/migrations/20240131131154_add_another_created_at_field/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Session" ADD COLUMN     "created_at" TEXT;
