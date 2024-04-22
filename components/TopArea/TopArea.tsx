3e36b3ed00d71800925602ff9366ad4d6a1bf045
Mathias Ebner <fhs47758@fh-salzburg.ac.at>
2024-04-22 10:40:35 +0200

    remove top-level milestones

--- components/TopArea/TopArea.tsx
+++ components/TopArea/TopArea.tsx
@@ -1,27 +1,7 @@
-import Link from "next/link";
 import Image from "next/image";
 import LogoDarkImg from '@/images/logo_dark.svg';
-
 import Box from '@mui/material/Box';
-import Stepper from '@mui/material/Stepper';
-import Step from '@mui/material/Step';
-import StepLabel from '@mui/material/StepLabel';
-import Typography from '@mui/material/Typography';
 import styles from "./TopArea.module.scss";
-
-const steps = [
-    { label: 'Submission Exposé', date: '26.04.2024' },
-    { label: 'Entry in Portfolio', date: '26.07.2024' },
-    { label: 'Update Portfolio', date: '20.08.2024' },
-    { label: 'Project fixation', date: '31.08.2024' },
-];
-
-const stepLabelStyle = {
-    ".mui-5340bo-MuiStepLabel-label.MuiStepLabel-alternativeLabel": {
-        marginTop: 0.75,
-        fontSize: 13,
-    },
-};
 
 export default function TopArea({ eventData }: { eventData: any }) {
     return (
@@ -35,17 +15,6 @@ export default function TopArea({ eventData }: { eventData: any }) {
                 <h3>{eventData?.name ? `Event: ${eventData?.name}` : ""}</h3>
             </a>
             <Box sx={{ width: '100%' }}>
-                {/* <Stepper activeStep={1} alternativeLabel sx={stepLabelStyle}>
-                    {steps.map((step, index) => (
-                        <Step key={index}>
-                            <StepLabel className={styles.milestone}>
-                                {step.label}
-                                <br></br>
-                                <Typography variant="caption">{step.date}</Typography>
-                            </StepLabel>
-                        </Step>
-                    ))}
-                </Stepper> */}
             </Box>
         </div>
     );
